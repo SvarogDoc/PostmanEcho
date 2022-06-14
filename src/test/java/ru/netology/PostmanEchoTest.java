a2314af2dd4fadf67600ffaa64122a88bbd33c2c
SvarogDoc <learnqai33@gmail.com>
2022-06-14 18:16:34 +0300

    Breaking the test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Здравствуй лошадь, я - Будённый!"))
+                .body("data", equalTo("Здравствуй Будённый, я - лошадь!"))
         ;
     }
 }
